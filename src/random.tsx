6905e1fc2074a263368502da4f56f132e807e702
devjiwonchoi <devjiwonchoi@gmail.com>
2024-06-05 19:11:30 +0900

    chore: apply prettier

--- src/random.tsx
+++ src/random.tsx
@@ -8,15 +8,17 @@ type Values = {
 
 export default function Random() {
   const handleSubmit = ({ prefix, length, includeGitCommand }: Values) => {
-    const branchLength = parseInt(length);
-    const randomValue = (Math.random() * 1e20).toString(36).slice(0, branchLength);
-    const branchName = `${prefix ? prefix + "/" : ""}${randomValue}`;
-    const gitCommand = includeGitCommand ? "git checkout -b " : "";
+    const branchLength = parseInt(length)
+    const randomValue = (Math.random() * 1e20)
+      .toString(36)
+      .slice(0, branchLength)
+    const branchName = `${prefix ? prefix + '/' : ''}${randomValue}`
+    const gitCommand = includeGitCommand ? 'git checkout -b ' : ''
 
-    const result = `${gitCommand}${branchName}`;
-    Clipboard.copy(result);
-    showToast({ title: `${branchName}`, message: "Copied to your clipboard!" });
-  };
+    const result = `${gitCommand}${branchName}`
+    Clipboard.copy(result)
+    showToast({ title: branchName, message: 'Copied to your clipboard!' })
+  }
 
   return (
     <Form
@@ -26,14 +28,19 @@ export default function Random() {
         </ActionPanel>
       }
     >
-      <Form.TextField id="prefix" title="Prefix" placeholder="Add prefix of the branch: (e.g. jwn/~)" storeValue />
+      <Form.TextField
+        id="prefix"
+        title="Prefix"
+        placeholder="Add prefix of the branch: (e.g. jwn/~)"
+        storeValue
+      />
       <Form.TextField id="length" title="Length" defaultValue="4" storeValue />
       <Form.Checkbox
         id="includeGitCommand"
-        title="Include Git Command"
+        title="Copy Git Command"
         label="Include `git checkout -b` when copying to clipboard"
         storeValue
       />
     </Form>
-  );
+  )
 }
