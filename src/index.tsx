e869dcba51bb912610c54588f5c43dd443cf7fee
devjiwonchoi <devjiwonchoi@gmail.com>
2024-06-05 19:11:17 +0900

    feat: add branch name

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,38 @@
-import { Form, ActionPanel, Action, showToast } from "@raycast/api";
+import { Form, ActionPanel, Action, showToast, Clipboard } from '@raycast/api'
 
 type Values = {
-  textfield: string;
-  textarea: string;
-  datepicker: Date;
-  checkbox: boolean;
-  dropdown: string;
-  tokeneditor: string[];
-};
+  branchType: string
+  prefix: string
+  description: string
+  snakeCase: boolean
+  prefixDate: boolean
+}
+
+function slugify(content: string, separator: string) {
+  return content.toLowerCase().replaceAll(' ', separator)
+}
 
 export default function Command() {
-  function handleSubmit(values: Values) {
-    console.log(values);
-    showToast({ title: "Submitted form", message: "See logs for submitted values" });
+  const handleSubmit = ({
+    branchType,
+    prefix,
+    description,
+    snakeCase,
+    prefixDate,
+  }: Values) => {
+    const separator = snakeCase ? '_' : '-'
+    const date = prefixDate
+      ? `${new Date().toISOString().split('T')[0]}${separator}`
+      : ''
+    let content = `${date}${prefix}${branchType}${description}`
+
+    if (snakeCase && content.includes('-')) {
+      content = content.replaceAll('-', '_')
+    }
+
+    const branchName = slugify(content, separator)
+    Clipboard.copy(branchName)
+    showToast({ title: branchName, message: 'Copied to your clipboard!' })
   }
 
   return (
@@ -23,17 +43,42 @@ export default function Command() {
         </ActionPanel>
       }
     >
-      <Form.Description
-        text="Generate a new branch name."
-      />
-      <Form.Dropdown id="dropdown" title="Branch Type">
-        <Form.Dropdown.Item value="none" title="none" />
-        <Form.Dropdown.Item value="feat" title="feat" />
-        <Form.Dropdown.Item value="fix" title="fix" />
-        <Form.Dropdown.Item value="refactor" title="refactor" />
+      <Form.Dropdown id="branchType" title="Branch Type" storeValue>
+        <Form.Dropdown.Item value="" title="-" />
+        <Form.Dropdown.Item value="feat/" title="feat" />
+        <Form.Dropdown.Item value="fix/" title="fix" />
+        <Form.Dropdown.Item value="refactor/" title="refactor" />
       </Form.Dropdown>
-      <Form.TextField id="textfield" title="Description" placeholder="Leave blank for a random branch name" />
-      <Form.Checkbox id="checkbox" title="Options" label="Include Date" storeValue />
+      <Form.TextField
+        id="description"
+        title="Description"
+        placeholder="Description of this branch"
+        storeValue
+      />
+      <Form.TextField
+        id="prefix"
+        title="Prefix"
+        placeholder="Add a custom prefix for the branch: (e.g. jiwon)"
+        storeValue
+      />
+      <Form.Checkbox
+        id="snakeCase"
+        title="Use `snake_case`"
+        label="Use `snake_case` instead of `kebab-case`"
+        storeValue
+      />
+      <Form.Checkbox
+        id="prefixDate"
+        title="Prefix Date"
+        label="Add today's date as a prefix (e.g. 2024-06-05-)"
+        storeValue
+      />
+      <Form.Checkbox
+        id="includeGitCommand"
+        title="Copy Git Command"
+        label="Include `git checkout -b` when copying to clipboard"
+        storeValue
+      />
     </Form>
-  );
+  )
 }
